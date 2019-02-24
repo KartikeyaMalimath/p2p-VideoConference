d56ced563c254ca231e3d131b6b5014e603aee43
KartikeyaMalimath <kartikeyapm@gmail.com>
2019-02-24 22:54:28 +0530

    new project

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Pusher = require('pusher');
+const app = express();
+
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Session middleware
+
+// Create an instance of Pusher
+const pusher = new Pusher({
+    appId: '721837',
+    key: '0404486f2460041495bc',
+    secret: '5fd2909f1f2140e288cb',
+    cluster: 'ap2',
+    encrypted: true
+});
+
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/index.html');
+});
+
+// get authentictation for the channel;
+app.post('/pusher/auth', (req, res) => {
+        const socketId = req.body.socket_id;
+        const channel = req.body.channel_name;
+        var presenceData = {
+            user_id: Math.random().toString(36).slice(2) + Date.now()
+        }
+        const auth = pusher.authenticate(socketId, channel, presenceData);
+        res.send(auth);
+});
+
+//listen on the app
+app.listen(3000, () => {
+    return console.log('Server is up on 3000')
+});
